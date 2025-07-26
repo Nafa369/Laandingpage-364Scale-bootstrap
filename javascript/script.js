c4e6e4a875a721b89c3f01c1e5071b7826397a25
Nafa369 <imamulhunafa.kmj@gmail.com>
2025-07-26 21:33:22 +0700

    update effect 26 juli 2025

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,16 @@ const faders = document.querySelectorAll(".fade-in-on-scroll");
 function handlescroll() {
     faders.forEach(el => {
         const rect = el.getBoundingClientRect();
-        if (rect.top <= window.innerHeight - 100) {
+        if (
+            rect.top < window.innerHeight && 
+            rect.bottom > 0
+        ) {
             el.classList.add('visible');
+        } else {
+            el.classList.remove('visible');
         }
     });
 }
 
 window.addEventListener('scroll', handlescroll);
 window.addEventListener('load', handlescroll);
-console.log("script loaded and scroll event listener added");
