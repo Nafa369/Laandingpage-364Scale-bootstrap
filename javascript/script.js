ea42637096cdd46eb6d7c6b04c693400fd4c0f2c
Nafa369 <imamulhunafa.kmj@gmail.com>
2025-08-01 22:39:36 +0700

    update di bagian effect dan penambahan box di koleksi

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,18 @@
+// mengambil semua elemen yang punya class fade in on scroll dan fade in scrol x
 const faders = document.querySelectorAll(".fade-in-on-scroll, .fade-in-scroll-x");
+// menyimpan posisi scroll
 let lastScrollTop = window.scrollY;
-
+// untuk cek elemen mana yang sedang terlihat di viewport
 function handlescroll(){
     const currentScroll = window.scrollY;
-
+    // ambil posisi dan ukuran elemen relatif terhadap viewport
     faders.forEach(el => {
         const rect = el.getBoundingClientRect();
         // memunculkan elemen ketika 20% masuk viewport
         // hilangkan ketika keluar dari vieqwport
-    const isVisible = (rect.top < window.innerHeight * 0.8) && (rect.bottom > window.innerHeight * 0.25) ;
+    const isVisible = (rect.top < window.innerHeight * 0.8) && (rect.bottom > window.innerHeight * 0.25); //
 
+    // meembuat animasi fade secara halus
         if (isVisible) {
             el.classList.add('visible');
         } else {
@@ -17,11 +20,13 @@ function handlescroll(){
         }
     });
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-}
-// Throtle function untuk optimal performance
+}; //function handscroll
+
+// Throtle function untuk optimal performance agar fungsi tidak terlalu sering di panggi dan memberatkan performa
 function throtle(func, limit){
     let lastFunc;
     let lastRan;
+
     return function() {
         const context = this;
         const args = arguments;
@@ -36,9 +41,10 @@ function throtle(func, limit){
                     lastRan = Date.now();
                 }
             }, limit - (Date.now() - lastRan));
-        }
-    }
-}
+        }; // else clear time out
+    }; //return function
+}; //function throtle
+
 // event listener throtle
 window.addEventListener('scroll', throtle(handlescroll, 100));
 window.addEventListener('load', handlescroll);
